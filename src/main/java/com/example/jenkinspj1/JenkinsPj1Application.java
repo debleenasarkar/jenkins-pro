8ca54773ab6006f17f1b9450af61e3448ae77fa4
debleenasarkar <ria.debleena@gmail.com>
2024-05-04 20:57:29 +0000

    added

---
+++ src/main/java/com/example/jenkinspj1/JenkinsPj1Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinspj1/JenkinsPj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
